6f76ff3a11a937a70f40831e6d0bcf6a40e4fb5c
Russell <russellthurman94@gmail.com>
2020-03-10 11:36:46 -0500

    Channelling fixes

--- dhar.js
+++ dhar.js
@@ -1,263 +1,279 @@
 Hooks.on("ready", () => {
 	if (game.settings.get("eis", "dharRules"))
 	{
-	  DiceWFRP.rollCastTest = (testData) =>
-	  {
-		let spell = testData.extra.spell;
-		let testResults = DiceWFRP.rollTest(testData);
+	  DiceWFRP.rollCastTest = function(testData) {
+    let spell = testData.extra.spell;
+    let testResults = this.rollTest(testData);
 
-		let miscastCounter = 0;
-		testData.function = "rollCastTest"
+    let miscastCounter = 0;
+	testData.function = "rollCastTest"
+	
+	let dhar = false;
+	if (WFRP4E.magicWind[spell.data.lore.value] == "Dhar")
+	  dhar = true;
 
-		let dhar = false;
-		if (WFRP4E.magicWind[spell.data.lore.value] == "Dhar")
-		  dhar = true;
+	if (spell.data.lore.value == "witchcraft")
+	  miscastCounter++;
 
-		if (spell.data.lore.value == "witchcraft")
-		  miscastCounter++;
+    // Partial channelling - reduce CN by SL so far
+    if (game.settings.get("wfrp4e", "partialChannelling"))
+    {
+      spell.data.cn.value -= spell.data.cn.SL;
+    }
+    // Normal Channelling - if SL has reached CN, CN is considered 0
+    else if (spell.data.cn.SL >= spell.data.cn.value)
+    {
+      spell.data.cn.value = 0;
+    }
 
-		if (dhar)
+	if (dhar)
+	{
+		if (testResults.roll.toString().includes("8"))
 		{
-		  if (testResults.roll.toString().includes("8"))
-		  {
-			testResults.other = "All living souls within Willpower yards suffer a Minor Corrupting Influence and a Minor Miscast; a single result applies to all those within the area affected."
+			testResults.other = game.i18n.localize("EiS.MinorDhar")
 			miscastCounter++
 
 			if (testResults.roll == 88)
 			{
-			  testResults.other = "All living souls within Willpower yards suffer a Major Corrupting Influence and a Major Miscast; a single result applies to all those within the area affected."
-			  miscastCounter++;
+				testResults.other = game.i18n.localize("EiS.MajorDhar")
+				miscastCounter++;
 			}
-		  }
-
-		  else if (testResults.roll % 11 == 0)
-		  {
-			testResults.other = "Counts as a minor corrupting influence"
-		  }
-
-		}
-		else if (testData.extra.malignantInfluence)
-		  if (Number(testResults.roll.toString().split('').pop()) == 8)
-			miscastCounter++;
-
-		if (game.settings.get("wfrp4e", "partialChannelling"))
-		{
-		  spell.data.cn.value -= spell.data.cn.SL;
-		}
-		else if (spell.data.cn.SL >= spell.data.cn.value)
-		{
-		  spell.data.cn.value = 0;
-		}
-		let slOver = (Number(testResults.SL) - spell.data.cn.value)
-
-		if (testResults.description.includes("Failure")) // Failed Test
-		{
-		  testResults.description = "Casting Failed"
-		  if (testResults.roll % 11 == 0 || testResults.roll == 100)
-		  {
-			testResults.extra.color_red = true;
-			miscastCounter++;
-		  }
-		}
-		else if (slOver < 0) // Successful test, but unable to cast
-		{
-		  testResults.description = "Casting Failed"
-
-		  if (testResults.roll % 11 == 0)
-		  {
-			testResults.extra.color_green = true;
-			testResults.description = "Casting Succeeded"
-			testResults.extra.critical = "Total Power"
-
-			if (!testData.extra.ID)
-			  miscastCounter++;
-		  }
-		}
-		else // Successful test, casted
-		{
-		  testResults.description = "Casting Succeeded"
-		  let overcasts = Math.floor(slOver / 2);
-		  if (dhar)
-			overcasts = slOver
-		  testResults.overcasts = overcasts;
-
-		  // If no ID
-		  if (testResults.roll % 11 == 0)
-		  {
-			testResults.extra.critical = "Critical Cast"
-			testResults.extra.color_green = true;
-
-			if (!testData.extra.ID)
-			  miscastCounter++;
-		  }
-
 		}
 
-		switch (miscastCounter)
+		else if (testResults.roll % 11 == 0)
 		{
-		  case 1:
-			if (testData.extra.ingredient)
-			  testResults.extra.nullminormis = "Minor Miscast"
-			else
-			  testResults.extra.minormis = "Minor Miscast"
-			break;
-		  case 2:
-			if (testData.extra.ingredient)
-			{
-			  testResults.extra.nullmajormis = "Major Miscast"
-			  testResults.extra.minormis = "Minor Miscast"
-			}
-			else
-			  testResults.extra.majormis = "Major Miscast"
-			break;
-		  case 3:
-			testResults.extra.majormis = "Major Miscast"
-			break;
+			testResults.other = game.i18n.localize("EiS.DoubleRolled")
 		}
 
-		if (testData.extra.ingredient)
-		  miscastCounter--;
-		if (miscastCounter < 0)
-		  miscastCounter = 0;
-		if (miscastCounter > 2)
-		  miscastCounter = 2
-
-		try
-		{
-		  if (testData.extra.spell.damage && testResults.description.includes("Succeeded"))
-			testResults.damage = Number(testResults.SL) +
-			Number(testData.extra.spell.damage)
-		}
-		catch (error)
-		{
-		  ui.notifications.error("Error calculating damage: " + error)
-		} // If something went wrong calculating damage, do nothing and continue
-
-
-		return testResults;
-	  }
-
-
-	  DiceWFRP.rollChannellTest = (testData, actor) =>
-	  {
-		let spell = testData.extra.spell;
-		let miscastCounter = 0;
-
-		let testResults = DiceWFRP.rollTest(testData);
-		testData.function = "rollChannellTest"
-
-		if (spell.data.lore.value == "witchcraft")
-		  miscastCounter++;
-
-		let dhar = false;
-		if (WFRP4E.magicWind[spell.data.lore.value] == "Dhar")
-		  dhar = true;
-		if (testData.extra.channellSkill && testData.extra.channellSkill.name.includes("Dhar"))
-		  dhar = true;
-
-		let SL = testResults.SL;
-
-
-		if (dhar)
-		{
-		  if (testResults.roll.toString().includes("8"))
-		  {
-			testResults.other = "All living souls within Willpower yards suffer a Minor Corrupting Influence and a Minor Miscast; a single result applies to all those within the area affected."
-			miscastCounter++
-
-			if (testResults.roll == 88)
-			{
-			  testResults.other = "All living souls within Willpower yards suffer a Major Corrupting Influence and a Major Miscast; a single result applies to all those within the area affected."
-			  miscastCounter++;
-			}
-		  }
-
-		  else if (testResults.roll % 11 == 0)
-		  {
-			testResults.other = "Counts as a minor corrupting influence"
-		  }
-
-		}
-
-		else if (testData.extra.malignantInfluence)
-		{
-		  if (Number(testResults.roll.toString().split('').pop()) == 8)
-			miscastCounter++;
-		}
-
-		if (testResults.description.includes("Failure")) // Failed Test
-		{
-		  // Optional Rule: If SL in extended test is -/+0, counts as -/+1
-		  if (Number(SL) == 0 && game.settings.get("wfrp4e", "extendedTests"))
-			SL = -1;
-
-		  testResults.description = "Channell Failed"
-		  if (testResults.roll % 11 == 0 || testResults.roll % 10 == 0 || testResults.roll == 100)
-		  {
-			testResults.extra.color_red = true;
-			miscastCounter += 2;
-		  }
-		}
-		else
-		{
-		  testResults.description = "Channell Succeeded"
-
-		  // Optional Rule: If SL in extended test is -/+0, counts as -/+1
-		  if (Number(SL) == 0 && game.settings.get("wfrp4e", "extendedTests"))
-			SL = 1;
-
-		  if (testResults.roll % 11 == 0)
-		  {
-			testResults.extra.color_green = true;
-			spell.data.cn.SL = spell.data.cn.value;
-			testResults.extra.criticalchannell = "Critical Channell"
-			if (!testData.extra.AA)
-			  miscastCounter++;
-		  }
-		}
-
-		// Add SL to CN and update actor
-		spell.data.cn.SL += Number(SL);
-		if (spell.data.cn.SL > spell.data.cn.value)
-		  spell.data.cn.SL = spell.data.cn.value;
-		else if (spell.data.cn.SL < 0)
-		  spell.data.cn.SL = 0;
-
-		actor.updateOwnedItem(
-		{
-		  id: spell.id,
-		  'data.cn.SL': spell.data.cn.SL
-		});
-
-		switch (miscastCounter)
-		{
-		  case 1:
-			if (testData.extra.ingredient)
-			  testResults.extra.nullminormis = "Minor Miscast"
-			else
-			  testResults.extra.minormis = "Minor Miscast"
-			break;
-		  case 2:
-			if (testData.extra.ingredient)
-			{
-			  testResults.extra.nullmajormis = "Major Miscast"
-			  testResults.extra.minormis = "Minor Miscast"
-			}
-			else
-			  testResults.extra.majormis = "Major Miscast"
-			break;
-		  case 3:
-			testResults.extra.majormis = "Major Miscast"
-			break;
-		}
-
-		if (testData.extra.ingredient)
-		  miscastCounter--;
-		if (miscastCounter < 0)
-		  miscastCounter = 0;
-		if (miscastCounter > 2)
-		  miscastCounter = 2
-		return testResults;
-	  }
 	}
-})
+	else if (testData.extra.malignantInfluence)
+		if (Number(testResults.roll.toString().split('').pop()) == 8)
+			miscastCounter++;
+
+    // Witchcraft automatically miscast
+    if (spell.data.lore.value == "witchcraft")
+      miscastCounter++;
+
+    // slOver is the amount of SL over the CN achieved
+    let slOver = (Number(testResults.SL) - spell.data.cn.value)
+
+    // Test itself was failed
+    if (testResults.description.includes("Failure"))
+    {
+      testResults.description = game.i18n.localize("ROLL.CastingFailed")
+      // Miscast on fumble
+      if (testResults.roll % 11 == 0 || testResults.roll == 100)
+      {
+        testResults.extra.color_red = true;
+        miscastCounter++;
+      }
+    }
+    else if (slOver < 0) // Successful test, but unable to cast due to not enough SL
+    {
+      testResults.description = game.i18n.localize("ROLL.CastingFailed")
+
+      // Critical Casting - succeeds only if the user chooses Total Power option (which is assumed)
+      if (testResults.roll % 11 == 0)
+      {
+        testResults.extra.color_green = true;
+        testResults.description = game.i18n.localize("ROLL.CastingSuccess")
+        testResults.extra.critical = game.i18n.localize("ROLL.TotalPower")
+
+        if (!testData.extra.ID)
+          miscastCounter++;
+      }
+    }
+
+    else // Successful test, casted - determine overcasts
+    {
+      testResults.description = game.i18n.localize("ROLL.CastingSuccess")
+      let overcasts = Math.floor(slOver / 2);
+      testResults.overcasts = overcasts;
+
+      if (testResults.roll % 11 == 0)
+      {
+        testResults.extra.critical = game.i18n.localize("ROLL.CritCast")
+        testResults.extra.color_green = true;
+
+        if (!testData.extra.ID)
+          miscastCounter++;
+      }
+
+	}
+	
+	if (miscastCounter > 2)
+		miscastCounter = 2
+
+    // Use the number of miscasts to determine what miscast it becomes (null<miscast> is from ingredients)
+    switch (miscastCounter)
+    {
+      case 1:
+        if (testData.extra.ingredient)
+          testResults.extra.nullminormis = game.i18n.localize("ROLL.MinorMis")
+        else
+          testResults.extra.minormis = game.i18n.localize("ROLL.MinorMis")
+        break;
+      case 2:
+        if (testData.extra.ingredient)
+        {
+          testResults.extra.nullmajormis = game.i18n.localize("ROLL.MajorMis")
+          testResults.extra.minormis = game.i18n.localize("ROLL.MinorMis")
+        }
+        else
+          testResults.extra.majormis = game.i18n.localize("ROLL.MajorMis")
+        break;
+      case 3:
+        testResults.extra.majormis = game.i18n.localize("ROLL.MajorMis")
+        break;
+    }
+
+    if (testData.extra.ingredient)
+      miscastCounter--;
+    if (miscastCounter < 0)
+      miscastCounter = 0;
+    if (miscastCounter > 2)
+      miscastCounter = 2
+
+    // Calculate Damage if the spell has it specified and succeeded in casting
+    try
+    {
+      if (testData.extra.spell.damage && testResults.description.includes(game.i18n.localize("ROLL.CastingSuccess")))
+        testResults.damage = Number(testResults.SL) +
+        Number(testData.extra.spell.damage)
+    }
+    catch (error)
+    {
+		ui.notifications.error(game.i18n.localize("Error.DamageCalc") + ": " + error)
+    } // If something went wrong calculating damage, do nothing and continue
+
+
+    return testResults;
+  }
+
+  DiceWFRP.rollChannellTest = function(testData, actor) {
+    let spell = testData.extra.spell;
+    let miscastCounter = 0;
+
+    let testResults = this.rollTest(testData);
+    testData.function = "rollChannellTest"
+
+	if (spell.data.lore.value == "witchcraft")
+	miscastCounter++;
+
+	let dhar = false;
+	if (WFRP4E.magicWind[spell.data.lore.value] == "Dhar")
+		dhar = true;
+	if (testData.extra.channellSkill && testData.extra.channellSkill.name.includes("Dhar"))
+		dhar = true;
+		
+	let SL = testResults.SL;
+
+	
+	if (dhar)
+	{
+		if (testResults.roll.toString().includes("8"))
+		{
+			testResults.other = game.i18n.localize("EiS.MinorDhar")
+			miscastCounter++
+
+			if (testResults.roll == 88)
+			{
+				testResults.other = game.i18n.localize("EiS.MajorDhar")
+			}
+		}
+
+		else if (testResults.roll % 11 == 0)
+		{
+			testResults.other = game.i18n.localize("EiS.DoubleRolled")
+		}
+	}
+
+	else if (testData.extra.malignantInfluence)
+	{
+	if (Number(testResults.roll.toString().split('').pop()) == 8)
+		miscastCounter++;
+	}
+    // Test itself was failed
+    if (testResults.description.includes(game.i18n.localize("Failure")))
+    {
+      // Optional Rule: If SL in extended test is -/+0, counts as -/+1
+      if (Number(SL) == 0 && game.settings.get("wfrp4e", "extendedTests"))
+        SL = -1;
+
+      testResults.description = game.i18n.localize("ROLL.ChannelFailed")
+      // Major Miscast on fumble
+      if (testResults.roll % 11 == 0 || testResults.roll % 10 == 0 || testResults.roll == 100)
+      {
+        testResults.extra.color_red = true;
+        miscastCounter += 2;
+      }
+    }
+    else // Successs - add SL to spell for further use
+    {
+      testResults.description = game.i18n.localize("ROLL.ChannelSuccess")
+
+      // Optional Rule: If SL in extended test is -/+0, counts as -/+1
+      if (Number(SL) == 0 && game.settings.get("wfrp4e", "extendedTests"))
+        SL = 1;
+
+      // Critical Channel - miscast and set SL gained to CN
+      if (testResults.roll % 11 == 0)
+      {
+        testResults.extra.color_green = true;
+        spell.data.cn.SL = spell.data.cn.value;
+        testResults.extra.criticalchannell = game.i18n.localize("ROLL.CritChannel")
+        if (!testData.extra.AA)
+          miscastCounter++;
+      }
+    }
+
+    // Add SL to CN and update actor
+    spell.data.cn.SL += Number(SL);
+    if (spell.data.cn.SL > spell.data.cn.value)
+      spell.data.cn.SL = spell.data.cn.value;
+    else if (spell.data.cn.SL < 0)
+      spell.data.cn.SL = 0;
+
+    actor.updateEmbeddedEntity("OwnedItem",
+    {
+      _id: spell._id,
+      'data.cn.SL': spell.data.cn.SL
+    });
+
+
+	if (miscastCounter > 2)
+		miscastCounter = 2
+
+    // Use the number of miscasts to determine what miscast it becomes (null<miscast> is from ingredients)
+    switch (miscastCounter)
+    {
+      case 1:
+        if (testData.extra.ingredient)
+          testResults.extra.nullminormis = game.i18n.localize("ROLL.MinorMis")
+        else
+          testResults.extra.minormis = game.i18n.localize("ROLL.MinorMis")
+        break;
+      case 2:
+        if (testData.extra.ingredient)
+        {
+          testResults.extra.nullmajormis = game.i18n.localize("ROLL.MajorMis")
+          testResults.extra.minormis = game.i18n.localize("ROLL.MinorMis")
+        }
+        else
+          testResults.extra.majormis = game.i18n.localize("ROLL.MajorMis")
+        break;
+      case 3:
+        testResults.extra.majormis = game.i18n.localize("ROLL.MajorMis")
+        break;
+	}
+	
+    if (testData.extra.ingredient)
+      miscastCounter--;
+    if (miscastCounter < 0)
+      miscastCounter = 0;
+    if (miscastCounter > 2)
+      miscastCounter = 2
+    return testResults;
+  }
+}
+});
